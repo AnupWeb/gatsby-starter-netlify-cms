b8004c58f000979c798283450b6ec5d789ba33b9
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-01-21 18:39:26 +0530

    bind content with cms

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,6 +7,11 @@ import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
+  title,
+  image,
+  heading,
+  description,
+  copyRight
 }) => (
   <div className="">
      <div className="selection_Of_examination">
@@ -14,7 +19,7 @@ export const IndexPageTemplate = ({
         <div className="row py-2">
             <div className="col-md-6 col-lg-6 col-xl-6 col-12 d-flex align-items-center justify-content-center justify-content-md-start">
                <div className="d-flex align-items-center ">
-                   <h5 className="text-white font-weight-bold mt-1">LUEZOID TECHNOLOGY</h5>
+<h5 className="text-white font-weight-bold mt-1">{title}</h5>
                </div>
             </div>
             <div className="col-md-6 col-lg-6 col-xl-6 col-12">
@@ -30,7 +35,7 @@ export const IndexPageTemplate = ({
             <div className="row">
                 <div className="col-12 col-md-8 col-lg-8 col-xl-8">
                     <div className="banner-image-section">
-                        <img src="assets/images/banner1.jpg" alt="" className="banner-image"/>
+                        <img src = {image} alt="" className="banner-image"/>
                     </div>
                 </div>
                 <div className="col-12 col-md-4 col-lg-4 col-xl-4">
@@ -67,20 +72,19 @@ export const IndexPageTemplate = ({
             <div className="row pt-5 pb-5 px-3">
                 <div className="col-12">
                     <div className="welcome-title">
-                        <h5 className="text-center text-white">WELCOME TO <span className="theme-text-color">LUEZOID TECHNOLOGY</span>, TEST PRACTICE CENTRE</h5>
+                        <h5 className="text-center text-white">{heading}</h5>
                     </div>
                 </div>
                 <div className="col-12 bg-white my-3"></div>
                 <div className="text-white welcome-text">
-                    <p>
-                        This Mock Test is to familiarize the students about processes of Computer Based Test (CBT),
-                        candidate can understand various processes of Computer Based Test (CBT) with the available mock
-                        test.
-                    </p>
+                    <p>{description}</p>
                 </div>
             </div>
         </div>
     </div>    
+    <div className="footer d-flex align-items-center justify-content-center py-3">
+        <div className="footer-text d-flex align-items-center">&copy; {copyRight} Luezoid Technology</div>
+    </div>
   </div>
 )
  
@@ -90,6 +94,11 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
+      image={frontmatter.image}
+      title={frontmatter.title}
+      heading={frontmatter.heading}
+      description={frontmatter.description}
+      copyRight={frontmatter.copyRight}
       />
     </Layout>
   )
@@ -101,6 +110,17 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        description
+        copyRight
+
       }
     }
   }
