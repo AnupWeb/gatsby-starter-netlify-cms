bbb1a7e12f575a10b5cd11db47614d41fdeb2813
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-01-21 15:57:41 +0530

    design landing page content

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,151 +7,93 @@ import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
 }) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
+  <div className="">
+     <div className="selection_Of_examination">
+    <div className="container">
+        <div className="row py-2">
+            <div className="col-md-6 col-lg-6 col-xl-6 col-12 d-flex align-items-center justify-content-center justify-content-md-start">
+               <div className="d-flex align-items-center ">
+                   <h5 className="text-white font-weight-bold mt-1">LUEZOID TECHNOLOGY</h5>
+               </div>
             </div>
-          </div>
+            <div className="col-md-6 col-lg-6 col-xl-6 col-12">
+                <div className=" d-flex flex-wrap justify-content-center justify-content-md-end  align-items-center ">                    
+                    <button className="btn text-white bg-transparent box-shadow-none">Log In</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+    </div>
+   <div className="main-content">
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-8 col-lg-8 col-xl-8">
+                    <div className="banner-image-section">
+                        <img src="assets/images/banner1.jpg" alt="" className="banner-image"/>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4 col-lg-4 col-xl-4">
+                    <div className="card select-test-Form p-4 bg-light-gray">
+                        <form>
+                            <div className="form-group">
+                                <label for="selectType">Select exam you would like to appear</label>
+                                <select  className="form-control" id="selectType" aria-describedby="emailHelp">
+                                    <option value="">--select--</option>
+                                    <option value="">abhdg1</option>
+                                    <option value="">abhdg2</option>
+                                    <option value="">abhdg3</option>
+                                    <option value="">abhdg4</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label for="paper">Paper</label>
+                                <select  className="form-control" id="paper" aria-describedby="emailHelp">
+                                    <option value="">--select--</option>
+                                    <option value="">Maths</option>
+                                    <option value="">English</option>
+                                    <option value="">snjskdla</option>
+                                    <option value="">abhdg4</option>
+                                </select>
+                            </div>
+                            <button type="submit" className="btn bg-blue col-12">Start Mock Test</button>
+                            <div className="form-group mt-2">
+                               <p> For LT Mock Tests of December 2019 onwards, please <a href="" className="text-danger">Click here*</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className="row pt-5 pb-5 px-3">
+                <div className="col-12">
+                    <div className="welcome-title">
+                        <h5 className="text-center text-white">WELCOME TO <span className="theme-text-color">LUEZOID TECHNOLOGY</span>, TEST PRACTICE CENTRE</h5>
+                    </div>
+                </div>
+                <div className="col-12 bg-white my-3"></div>
+                <div className="text-white welcome-text">
+                    <p>
+                        This Mock Test is to familiarize the students about processes of Computer Based Test (CBT),
+                        candidate can understand various processes of Computer Based Test (CBT) with the available mock
+                        test.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>    
   </div>
 )
-
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
-
+ 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
   )
-}
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
-
+} 
 export default IndexPage
 
 export const pageQuery = graphql`
@@ -159,34 +101,6 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
